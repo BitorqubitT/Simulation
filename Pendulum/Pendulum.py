e9d8ead2db9da6bb369d9bfd5b40f75b0cd6676a
BitorqubitT <thierryars2@hotmail.com>
2020-05-02 01:08:42 +0200

    Cleaned

--- Pendulum/Pendulum.py
+++ Pendulum/Pendulum.py
@@ -1,28 +1,11 @@
 import math
 import pygame
 
-
-#heat equation
 #Some chaotic system first in 2d then 3d?
 #Single pendulum, THen a double, Then with parameters and maybe another graph next to it.
 
-background_colour = (255, 255, 255)
-width, height = (700, 700)
-
-"""def trajectory(x, y):
-    pos = []
-    pos.append([x, y])
-    print(pos)
-    length = 0
-    while length <= len(pos):
-        pygame.draw.line(screen, (0, 0, 255), (pos[length][0], pos[length][1]), (pos[length][0], pos[length][1]))
-        length += 1
-        if length == len(pos):
-            break
-"""
-
 class Pendulum():
-    def __init__(self, x, y, size, length, screen_width, screen_height):
+    def __init__(self, x, y, size, length, screen_width, screen_height, screen):
         self.x = x
         self.y = y
         self.colour = (0, 0, 255)
@@ -36,11 +19,13 @@ class Pendulum():
         self.gravity = 9.8281
         self.velocity = 0
         self.damping = 0.99
+        self.screen = screen
+
 
     def display(self):
-        pygame.draw.circle(screen, self.colour, ((int(self.screen_width)), (int(self.screen_height))), 5, self.thickness)
-        pygame.draw.line(screen, self.colour, ((int(self.screen_width)), (int(self.screen_height))), (int(self.x), int(self.y)),self.thickness)
-        pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
+        pygame.draw.circle(self.screen, self.colour, ((int(self.screen_width)), (int(self.screen_height))), 5, self.thickness)
+        pygame.draw.line(self.screen, self.colour, ((int(self.screen_width)), (int(self.screen_height))), (int(self.x), int(self.y)),self.thickness)
+        pygame.draw.circle(self.screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
         self.x = self.length * math.sin(self.angle) + self.screen_width
         self.y = self.length * math.cos(self.angle) + self.screen_height
 
@@ -52,22 +37,3 @@ class Pendulum():
         self.velocity += acceleration
         self.angle += self.velocity
         self.velocity *= self.damping
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Pendulum')
-
-clock = pygame.time.Clock()
-running = True
-pendulum = Pendulum(350, 390, 10, 100, 350, 350)
-
-while running:
-    clock.tick(50)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    else:
-        screen.fill(background_colour)
-        pendulum.display()
-        pendulum.move()
-       # in a real game use .update() which is less taxing.
-        pygame.display.flip()
--- Pendulum/main.py
+++ Pendulum/main.py
@@ -1,23 +1,22 @@
-import Pendulum
+from Pendulum import Pendulum
 import pygame
 
 def main():
-    background_colour = (255, 255, 255)
-    width, height = (700, 700)
-    screen = pygame.display.set_mode((width, height))
+    BACKGROUNDC = (255, 255, 255)
+    WIDTH, HEIGHT = (700, 700)
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Pendulum')
 
     clock = pygame.time.Clock()
     running = True
-    pendulum = Pendulum(350, 390, 10, 100, width/2 , height/2, screen)
+    pendulum = Pendulum(350, 390, 10, 100, WIDTH/2 , HEIGHT/2, screen)
     while running:
         clock.tick(50)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         else:
-            screen.fill(background_colour)
-            pendulum.pivot()
+            screen.fill(BACKGROUNDC)
             pendulum.display()
             pendulum.move()
            # in a real game use .update() which is less taxing.
